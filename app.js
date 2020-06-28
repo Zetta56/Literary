eb5510a90c45a8b6be8e31a8badb6bc3f57a9dc4
root <ramonli413@gmail.com>
2020-06-28 19:35:16 +0000

    refactor routes, add show page and pagination

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser"),
-	mongoose = require("mongoose"),
-	methodOverride = require("method-override"),
-	flash = require("connect-flash"),
-	moment = require("moment"),
-	session = require("express-session"),
-	passport = require("passport"),
-	LocalStrategy = require("passport-local"),
-	passportLocalMongoose = require("passport-local-mongoose");
+	  app = express(),
+	  bodyParser = require("body-parser"),
+	  mongoose = require("mongoose"),
+	  methodOverride = require("method-override"),
+	  flash = require("connect-flash"),
+	  moment = require("moment"),
+	  session = require("express-session"),
+	  passport = require("passport"),
+	  LocalStrategy = require("passport-local"),
+	  passportLocalMongoose = require("passport-local-mongoose");
 
 const Piece = require("./models/piece");
+
+const piecesRoutes = require("./routes/pieces")
 
 require("dotenv").config();
 mongoose.connect(process.env.DatabaseURL || "mongodb://localhost/literary", {useNewUrlParser: true, useUnifiedTopology: true})
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"))
 app.use(flash());
 app.locals.moment = moment;
 
@@ -31,28 +34,10 @@ app.use((req, res, next) => {
 	next();
 })
 
+app.use("/pieces", piecesRoutes)
+
 app.get("/", (req, res) => {
 	res.render("landing");
-});
-
-app.get("/literature", async (req, res) => {
-	let pieces = await Piece.find({});
-	res.render("literature/index", {pieces: pieces});
-});
-
-app.get("/literature/new", (req, res) => {
-	res.render("literature/new");
-});
-
-app.post("/literature", async (req, res) => {
-	try {
-		await Piece.create(req.body.piece);
-		req.flash("success", "Piece successfully uploaded!");
-		res.redirect("/literature");
-	} catch(err) {
-		req.flash("error", err.message);
-		res.redirect("/back");
-	};
 });
 
 app.listen(process.env.PORT || 3000, () => {
---
+++ routes/pieces.js
@@ -0,0 +1,51 @@
+const express = require("express"),
+	  app = express(),
+	  router = express.Router(),
+	  Piece = require("../models/piece");
+
+router.get("/", async (req, res) => {
+	const max = 12,
+		  pageQuery = parseInt(req.query.page),
+		  pageNumber = pageQuery ? pageQuery: 1;
+	let pieces = await Piece.find({}).skip((max * pageNumber) - max).limit(max).exec();
+	let matches = await Piece.countDocuments().exec();
+	res.render("pieces/index", {pieces: pieces, current: pageNumber, pages: Math.ceil(matches / max)});
+});
+
+router.get("/new", (req, res) => {
+	res.render("pieces/new");
+});
+
+router.post("/", async (req, res) => {
+	try {
+		await Piece.create(req.body.piece);
+		req.flash("success", "Piece successfully uploaded!");
+		res.redirect("/pieces");
+	} catch(err) {
+		req.flash("error", err.message);
+		res.redirect("back");
+	};
+});
+
+router.get("/:id", async(req, res) => {
+	try {
+		let foundPiece = await Piece.findById(req.params.id);
+		res.render("pieces/show", {piece: foundPiece});
+	} catch(err) {
+		req.flash("error", err.message);
+		res.redirect("back");
+	};
+});
+
+router.delete("/:id", async (req, res) => {
+	try {
+		await Piece.findByIdAndDelete(req.params.id);
+		req.flash("success", "Piece successfully removed.")
+		res.redirect("/pieces");
+	} catch(err) {
+		req.flash("error", err.message);
+		res.redirect("back");
+	}
+})
+
+module.exports = router;
